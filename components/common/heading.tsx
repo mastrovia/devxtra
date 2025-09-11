d6f36de48b64c9a1c67dd8decd2e39a65761d832
Remin T Roy <86014784+remintroy@users.noreply.github.com>
2025-09-11 20:29:08 +0530

    refactor: standardize quotation marks and improve code formatting in heading.tsx

--- components/common/heading.tsx
+++ components/common/heading.tsx
@@ -1,24 +1,29 @@
-import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
-import clsx from "clsx";
-import { cn } from "@/lib/utils";
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import clsx from 'clsx';
+import { cn } from '@/lib/utils';
 
 type HeadingProps = {
   tag?: React.ReactNode;
   subtitle?: React.ReactNode;
   className?: string;
   title?: string;
-  align?: "start" | "end" | "center";
+  align?: 'start' | 'end' | 'center';
   classnamea?: string;
 };
 
-export function Tag({ className, children, asChild, ...props }: React.AllHTMLAttributes<HTMLDivElement> & { asChild?: boolean }) {
-  const Comp = asChild ? Slot : "h3";
+export function Tag({
+  className,
+  children,
+  asChild,
+  ...props
+}: React.AllHTMLAttributes<HTMLDivElement> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot : 'h3';
 
   return (
     <Comp
       className={clsx(
-        "flex min-h-7 items-center justify-center gap-2 rounded-full bg-secondary px-3.5 pb-px text-sm font-medium text-tertiary md:text-base",
+        'flex min-h-7 items-center justify-center gap-2 rounded-full bg-secondary px-3.5 pb-px text-sm font-medium text-tertiary md:text-base',
         className
       )}
       {...props}
@@ -28,19 +33,21 @@ export function Tag({ className, children, asChild, ...props }: React.AllHTMLAtt
   );
 }
 
-export function Heading({ tag, subtitle, align = "center", ...props }: HeadingProps) {
+export function Heading({ tag, subtitle, align = 'center', ...props }: HeadingProps) {
   return (
-    <div className={cn("flex items-center flex-col gap-3", `items-${align} text-${align}`, props.classnamea)}>
+    <div className={cn('flex items-center flex-col gap-3', `items-${align} text-${align}`, props.classnamea)}>
       {tag ? <Tag>{tag}</Tag> : null}
       <div
         className={cn(
-          "flex max-w-[800px] flex-col justify-center gap-1",
-          "[&>*]:text-pretty [&>*]:text-3xl [&>*]:font-medium md:[&>*]:text-4xl"
+          'flex max-w-[800px] flex-col justify-center gap-1',
+          '[&>*]:text-pretty [&>*]:text-3xl [&>*]:font-medium md:[&>*]:text-4xl'
         )}
       >
         <h1 {...props}>{props?.title}</h1>
       </div>
-      {subtitle ? <p className="max-w-screen-md text-pretty font-light text-tertiary text-lg">{subtitle}</p> : null}
+      {subtitle ? (
+        <p className="max-w-screen-md text-pretty font-light text-muted-foreground text-lg">{subtitle}</p>
+      ) : null}
     </div>
   );
 }
