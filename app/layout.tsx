fac7887b34a244b69a5b57ec9bfd6fed99c4c951
maheswar-dileep <maheaswerd@gmail.com>
2025-09-11 21:49:23 +0530

    refactor: standardize quotation marks and improve formatting in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import { Header } from "@/components/header";
-import Footer from "@/components/footer";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Header } from '@/components/header';
+import Footer from '@/components/footer';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "DevXtra - Learn. Build. Grow.",
+  title: 'DevXtra - Learn. Build. Grow.',
   description:
-    "Learn, build, and grow with DevXtra. Access expert-led courses, hands-on projects, and a thriving community to elevate your tech skills and accelerate your career.",
-  manifest: "/site.webmanifest",
-  icons: "/favicon.ico",
+    'Join DevXtra to learn fullstack web development, AI-assisted coding, mobile app development, and real-world startup projects. Gain hands-on experience, build your portfolio, earn while learning, and prepare for a successful tech career.',
+  manifest: '/site.webmanifest',
+  icons: '/favicon.ico',
 };
 
 export default function RootLayout({
