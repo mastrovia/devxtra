d7f63de01b36539cb0331194a35bc45c7df0bbb6
Remin T Roy <86014784+remintroy@users.noreply.github.com>
2025-09-12 21:32:57 +0530

    refactor: uncomment FeaturesListSection and update its usage for improved clarity

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import TechStacksSection from './_sections/companies';
 import FaqsSection from './_sections/faq';
 import FeaturesBigImageSection from './_sections/features-big-image';
 import FeaturesGridSection from './_sections/features-grid';
-import FeaturesListSection from './_sections/features-list';
+// import FeaturesListSection from './_sections/features-list';
 import FeaturesSideBySideSection from './_sections/features-sidebyside';
 import HeroSection from './_sections/hero';
 // import Testimonials from './_sections/testimonials';
@@ -14,7 +14,7 @@ export default function Home() {
     <main className="flex flex-col gap-14">
       <HeroSection id="main" />
       <TechStacksSection id="tech-stacks" />
-      <FeaturesListSection id="out-promise" />
+      {/* <FeaturesListSection id="out-promise" /> */}
       <FeaturesGridSection id="what-you-will-learn" />
       <CalloutSection id="join-devxtra-now" />
       <FeaturesBigImageSection />
