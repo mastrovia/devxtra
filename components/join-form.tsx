b267873cf6757199b2854e5c4dcc4ec611cec4c8
maheswar-dileep <maheaswerd@gmail.com>
2025-10-25 16:19:02 +0530

    fix: update JoinForm layout to hide offline course question

--- components/join-form.tsx
+++ components/join-form.tsx
@@ -133,7 +133,7 @@ export default function JoinForm() {
                   </SelectContent>
                 </Select>
               </div>
-              <div className="flex flex-col gap-2">
+              <div className="hidden flex-col gap-2">
                 <Label htmlFor="name-input" className="font-medium">
                   Are you okay for offline course ?
                 </Label>
